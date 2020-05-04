7922747934014bfe4ece4001b5185b23e013d489
timgoeller <tim.goeller@posteo.de>
2020-05-04 17:48:53 +0200

    fix deduplication by setting connection encoding

--- backend/peerwolf.js
+++ backend/peerwolf.js
@@ -14,38 +14,31 @@ function Peerwolf () {
     ipcMain.on('create-game', (event, arg) => {
       let topic = crypto.randomBytes(32)
       console.log("TOPIC: " + topic.toString('hex'))
-      joinSwarm(topic)
+      joinSwarm(topic, event)
     })
 
     ipcMain.on('join-game', (event, arg) => {
-      joinSwarm(Buffer.from(arg, 'hex'))
+      joinSwarm(Buffer.from(arg, 'hex'), event)
     })
   }
 
-  function joinSwarm(topic) {
+  function joinSwarm(topic, renderEvent) {
     const swarm = hyperswarm({maxPeers: 40})
 
     swarm.join(topic, {lookup: true,announce: true})
 
     swarm.on('connection', (connection, details) => {
-      // socket.on('share-id', remoteID => {
-      //   console.log(remoteID)
-      //   console.log(clientID)
-      //   if(remoteID != clientID) {
-      //     var dropped = details.deduplicate(clientID, remoteID)
-      //     console.log(dropped)
-      //   }
-      // })
-      // socket.emit('share-id', clientID)
-
-      console.log('My ID: ' + clientID.toString('hex'));
 
       connection.on('data', (data) => {
         //only data that can be recieved here is a client's id
         var dropped = details.deduplicate(clientID, Buffer.from(data, 'hex'))
         console.log(dropped)
+        if(!dropped) {
+          renderEvent.sender.send('new-peer', {connection: connection, details: details})
+        }
       });
 
+      connection.setEncoding('utf8')
       connection.write(clientID.toString('hex')); //VERY simple handshake to handle deduplication
     })
   }
